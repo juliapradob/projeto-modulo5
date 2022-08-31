c6bfde1dc5affd5f353c4c3f4b3c53babbb9c676
Julia Prado <99263001+juliapradob@users.noreply.github.com>
2022-08-30 23:51:38 -0300

    build: prepara arquivos para divisão de branches e git pull

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,7 @@
+import S from './Button.module.css'
+
+export const Botao = () => {
+    return(
+        <button className={S.botao}>teste</button>
+    )
+}
---
+++ src/components/Formulario/Formulario.jsx
@@ -0,0 +1,9 @@
+import S from './Formulario.module.css'
+
+export const Formulario = () => {
+    return(
+        <form className={S.formulario}>
+            
+        </form>
+    )
+}
