b356698dfafc71b77db0f8c7b9047590558d0bc5
Julia Prado <99263001+juliapradob@users.noreply.github.com>
2022-08-30 23:24:09 -0300

    build: prepara pastas e rotas na main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Header } from './components/Header/Header'
+import { Home } from './pages/Home/Home'
+import { Clientes } from './pages/Clientes/Clientes'
+import { Livros } from './pages/Livros/Livros'
+import { Informatica } from './pages/Informatica/Informatica'
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Header />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/clientes' element={<Clientes/>}/>
+        <Route path='/livros' element={<Livros/>}/>
+        <Route path='/informatica' element={<Informatica/>}/>
+      </Routes>
+    </BrowserRouter>
+    <Footer />
   </React.StrictMode>
 )
