0474f4c8a11ae736540939ab40e86e2239a2b667
Kahrinita <karinamottamaciel@hotmail.com>
2022-09-01 23:29:34 -0300

    Fix: Atualizado

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import {Botao} from '../Button/Button' 
 
-export const Info = ({nome}) => {
+export const Info = ({nome, email, telefone, cpf}) => {
   return (
-    <div><p>{nome}</p><Botao texto = "Editar"></Botao>
-    <Botao texto = "Deletar"></Botao>
+    <div>
+        <table>
+            <tr>
+                <th>Nome</th>
+                <th>Email</th>
+                <th>Telefone</th>
+                <th>CPF</th>
+            </tr>
+
+            <tr>
+                <td>{nome}</td>
+                <td>{email}</td>
+                <td>{telefone}</td>
+                <td>{cpf}</td>
+            </tr> 
+            
+           <tr>
+                <Botao texto = "Editar"></Botao>
+                <Botao texto = "Deletar"></Botao>
+            
+            </tr> 
+        </table>
     </div>
   )
 }
