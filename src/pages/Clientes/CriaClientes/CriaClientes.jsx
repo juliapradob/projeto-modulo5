1c3ce7a24d5ecbb89bf2a13ca4c50963a7046360
Julia Prado <99263001+juliapradob@users.noreply.github.com>
2022-09-06 11:05:48 -0300

    fix: arruma CSS de adição e deleção de clientes

--- src/pages/Clientes/CriaClientes/CriaClientes.jsx
+++ src/pages/Clientes/CriaClientes/CriaClientes.jsx
@@ -1,24 +1,22 @@
 import { useNavigate, useParams } from 'react-router-dom'
 import { Form } from '../Form/Form'
 import { CriaCliente } from '../../../services/requisicaoClientes'
+import S from './CriaClientes.module.css'
 
 export const CriaClientes = () => {
     const navigate = useNavigate()
     const params = useParams()
 
     return (
-        <div>
-        <h1>{params.id ? "Editar Usuário" : "Cadastrar Usuário"}</h1>
-        <div>
+        <div className={S.formulario}>
+            <h1>{params.id ? "Editar Usuário" : "Cadastrar Usuário"}</h1>
             <Form temId={params.id} aoTerminar={(cliente) => {
                 CriaCliente(cliente).then(response => {
                     navigate('/clientes')
                 }).catch(error => {
                     alert('Erro na criação do cliente')
-                    console.log(cliente)
                 })
             }}/>
-        </div>
         </div>
     )
 }
--- src/pages/Clientes/EditaClientes/EditaClientes.jsx
+++ src/pages/Clientes/EditaClientes/EditaClientes.jsx
@@ -1,13 +1,14 @@
 import { useNavigate, useParams } from 'react-router-dom'
 import { Form } from '../Form/Form'
 import { EditaCliente } from '../../../services/requisicaoClientes'
+import S from './EditaClientes.module.css'
 
 export const EditaClientes = () => {
     const navigate = useNavigate()
     const params = useParams()
 
     return (
-        <div>
+        <div className={S.formulario}>
             <h1>{params.id ? "Editar Usuário" : "Cadastrar Usuário"}</h1>
             <Form temId={params.id} aoTerminar={(cliente) => {
                 EditaCliente(cliente).then(response => {
