e40d6ef408a9c256a6647c553316b3db34f2cd98
Camila Reis <79461028+cxavier6@users.noreply.github.com>
2022-09-07 17:16:50 -0300

    feat: adiciona criação e edição de livros

---
+++ src/pages/Livros/CriaEditaLivros/CriaEditaLivros.jsx
@@ -0,0 +1,44 @@
+import { useNavigate, useParams } from 'react-router-dom'
+import { Form } from '../Form/Form'
+import { CriaLivro, EditaLivro, RequisitaLivroPorId } from '../../../services/requisicaoLivros'
+import { useEffect, useState } from 'react'
+
+import S from './CriaEditaLivros.module.css'
+
+export const CriaEditaLivros = () => {
+    const navigate = useNavigate()
+    const params = useParams()
+    const [editarLivro, setEditarLivro] = useState({})
+
+    useEffect(() => {
+        if (params.id) {
+            RequisitaLivroPorId(params.id).then(({ data }) => {
+                setEditarLivro(data)
+            }).catch(error => {
+                alert("Erro ao obter livro")
+            })
+        }
+    }, [])
+
+    return (
+        <div className={S.formulario}>
+            <h1>{params.id ? "Editar livro" : "Cadastrar livro"}</h1>
+            <Form temId={params.id} editarLivro={editarLivro} aoTerminar={(livro) => {
+                if(params.id) {
+                    console.log(livro)
+                    EditaLivro(params.id, {titulo: livro.titulo, autor: livro.autor, genero: livro.genero, valor: livro.valor}).then(response => {
+                        navigate('/livros')
+                    }).catch(error => {
+                        alert('Erro na edição do livro')
+                    })
+                } else {
+                    CriaLivro(livro).then(response => {
+                        navigate('/livros')
+                    }).catch(error => {
+                        alert('Erro na criação do livro')
+                    })
+                }
+            }}/>
+        </div>
+    )
+}
