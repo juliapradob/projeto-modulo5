89ae5809333737a4322d924a5f6c8170ea8be7ee
Julia Prado <99263001+juliapradob@users.noreply.github.com>
2022-09-08 22:31:56 -0300

    fix: ajusta rotas

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import { Footer } from './components/Footer/Footer'
 import { Home } from './pages/Home/Home'
 import { HomeClientes } from './pages/Clientes/HomeClientes'
 import { CriaEditaClientes } from './pages/Clientes/CriaEditaClientes/CriaEditaClientes'
+import { HomeLivros } from './pages/Livros/HomeLivros'
+import { CriaEditaLivros } from './pages/Livros/CriaEditaLivros/CriaEditaLivros'
 import { Login } from './pages/Login/Login'
 import './index.css'
 
@@ -19,6 +21,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path='/clientes' element={<HomeClientes/>}/>
         <Route path='/cria-cliente' element={<CriaEditaClientes/>}/>
         <Route path='/edita-cliente/:id' element={<CriaEditaClientes/>}/>
+        <Route path='/livros' element={<HomeLivros/>}/>
+        <Route path='/cria-livro' element={<CriaEditaLivros/>}/>
+        <Route path='/edita-livro/:id' element={<CriaEditaLivros/>}/>
       </Routes>
     </main>  
     <Footer />
