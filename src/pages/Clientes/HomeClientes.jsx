ecbb39b11e7cdc99d7b6413ed00a76c1a0492913
Julia Prado <99263001+juliapradob@users.noreply.github.com>
2022-09-08 23:05:36 -0300

    fix: resolve conflito - jsx HomeClientes

--- src/pages/Clientes/HomeClientes.jsx
+++ src/pages/Clientes/HomeClientes.jsx
@@ -1,6 +1,5 @@
 import { RequisitaCliente, DeletaCliente } from "../../services/requisicaoClientes"
 import { useEffect, useState } from "react"
-import { Header } from "../../components/Header/Header";
 import { Loading } from "../../components/Loading/Loading";
 import { Tabela } from "./Tabela/Tabela"
 import { Botao } from "../../components/Button/Button";
@@ -29,7 +28,6 @@ export function HomeClientes() {
 
     return (
         <div>
-            <Header/>
             <div className={S.cadastroContainer}>
                 <h1>Clientes</h1>
                 <Botao texto="Adicionar cliente" navegação={true} clique="/cria-cliente" />
